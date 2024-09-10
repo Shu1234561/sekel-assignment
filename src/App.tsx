597903fb37f9b9780516004b3ce4648f804b6ad9
Shubham01234 <shubham.k@rejolut.com>
2024-09-10 17:40:32 +0530

    created a card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardComponent from './components/CardComponent';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getProducts = () => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log("data::", data);
+  
   return (
-    <div >
-      <h1 className='bg-orange-600'>Welcome</h1>
-      <p className='bg-red-500'>why</p>
+    <div className="mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {data.map((item, index) => (
+        <CardComponent item={item}/>
+      ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,33 @@
+import { ProductDetail } from "../Common/interface";
+
+interface CardComponentProps {
+    item: ProductDetail;
+}
+
+const CardComponent = ({ item }: CardComponentProps) => {
+    return (
+        <div className='w-[300px] border border-gray-200 rounded-md mt-4 shadow-md p-4 cursor-pointer'>
+            {/* <img src='https://i.imgur.com/cSytoSD.jpeg' alt='img' className='rounded-md m-auto'/>
+            <img src={item?.images[0]} alt='img' className='rounded-md m-auto'/> */}
+            {item.images.length > 1 ?
+                    item.images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={item.title}
+                            style={{ width: '200px', margin: '10px' }} // Adjust width and margin as needed
+                        />
+                    ))
+                :
+            <img src={item?.images[0]} alt='img' className='rounded-md m-auto'/>
+        }
+            <h3 className='text-[16px] font-bold text-[#282c3f] mb-1.5 overflow-hidden text-ellipsis whitespace-nowrap' title='Change the title'>{item.title}</h3>
+            <div className='text-sm font-bold text-[#282c3f] whitespace-nowrap my-2'>
+                <span>{`Rs.${item?.price}`}</span>
+            </div>
+            {/* <p className='text-sm text-[#5a5a5a] leading-relaxed'>Elevate your casual wardrobe with our Classic Black Hooded Sweatshirt. Made from high-quality, soft fabric that ensures comfort and durability, this hoodie features a spacious kangaroo pocket and an adjustable drawstring hood. Its versatile design makes it perfect for a relaxed day at home or a casual outing.</p> */}
+        </div>
+    )
+}
+
+export default CardComponent;
